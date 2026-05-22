76f83bd7cbd5b83c1eb2a218307cf729e29f0d5b
Kaushik Patil <280812057+workkwithkaushik-del@users.noreply.github.com>
2026-05-22 16:28:43 +0530

    Added new files

--- netlify/functions/server.js
+++
@@ -1,9 +0,0 @@
-import server from '../../dist/server/server.js';
-
-export default async (request, context) => {
-  return server.fetch(request, process.env, context);
-};
-
-export const config = {
-  path: "/*"
-};
